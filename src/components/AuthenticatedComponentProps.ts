e48475473443f21c254c3015e164ad960683c6c1
gpi <gpimpale29@gmail.com>
2023-01-14 01:26:18 -0800

    told react to stop being annoying

--- src/components/AuthenticatedComponentProps.ts
+++ src/components/AuthenticatedComponentProps.ts
@@ -4,8 +4,6 @@ import { Branding } from '@innexgo/common-react-components';
 export default interface AuthenticatedComponentProps {
   // other branding stuff
   branding: Branding,
-  // url to the auth-server domain
-  authServerUrl: string,
   // api key
   apiKey: ApiKey,
   // function to set the api key
--- src/components/AuthenticatedComponentRenderer.tsx
+++ src/components/AuthenticatedComponentRenderer.tsx
@@ -1,46 +1,81 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Spinner } from "react-bootstrap";
 import { ApiKey, Email } from "@innexgo/frontend-auth-api";
 import AuthenticatedComponentProps from '../components/AuthenticatedComponentProps';
 import { Branding } from '@innexgo/common-react-components';
 import { useSearchParams } from "react-router-dom";
+import { Async } from "react-async";
+import WaitingPage from "@innexgo/common-react-components/lib/components/SimplePage";
 
 export interface AuthenticatedComponentRendererProps {
   branding: Branding,
-  authServerUrl: string,
   component: React.ComponentType<AuthenticatedComponentProps>
   apiKey: ApiKey | null,
-  setApiKey: (data: ApiKey | null) => void
+  setApiKey: (data: ApiKey | null) => void,
+  authServerUrlFn: () => Promise<string>,
 }
 
-function AuthenticatedComponentRenderer({ branding, component: AuthenticatedComponent, apiKey, authServerUrl, setApiKey, }: AuthenticatedComponentRendererProps) {
-  const isAuthenticated = apiKey !== null &&
-    apiKey.creationTime + apiKey.duration > Date.now() &&
-    apiKey.apiKeyKind === "VALID";
+interface NotAuthenticatedHandlerProps {
+  setApiKey: (data: ApiKey | null) => void,
+  authServerUrl: string,
+}
 
-  React.useEffect(() => {
-    if (!isAuthenticated) {
+class NotAuthenticatedHandler extends React.Component<NotAuthenticatedHandlerProps> {
+  private alreadyMoved: boolean;
+  constructor(props: NotAuthenticatedHandlerProps) {
+    super(props);
+    this.alreadyMoved = false;
+  }
+
+  componentDidMount(): void {
+    if (!this.alreadyMoved) {
       const searchParams = new URLSearchParams(window.location.search);
       // if not authenticated then we need to check if we have a query string containing the api key.
       const apiKeyJson = searchParams.get('apiKey');
-
       if (apiKeyJson === null) {
         // if null we need to redirect to the auth site to login
-        let url = new URL('/login', authServerUrl);
+        let url = new URL('/login', this.props.authServerUrl);
         url.searchParams.append('src', window.location.href);
         window.location.replace(url);
       } else {
         // if not null then we set api key and restore parameters
-        setApiKey(JSON.parse(apiKeyJson));
-        window.location.replace(searchParams.get("src")!);
+        this.props.setApiKey(JSON.parse(apiKeyJson));
+        window.history.replaceState(null, '', searchParams.get("src")!);
       }
+      this.alreadyMoved = true;
     }
-  }, [isAuthenticated]);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+function AuthenticatedComponentRenderer(props: AuthenticatedComponentRendererProps) {
+  const isAuthenticated = props.apiKey !== null &&
+    props.apiKey.creationTime + props.apiKey.duration > Date.now() &&
+    props.apiKey.apiKeyKind === "VALID";
 
   if (isAuthenticated) {
-    return <AuthenticatedComponent apiKey={apiKey!} setApiKey={setApiKey} authServerUrl={authServerUrl} branding={branding} />
+    return <props.component apiKey={props.apiKey!} setApiKey={props.setApiKey} branding={props.branding} />
   } else {
-    return null;
+    return <Async promiseFn={props.authServerUrlFn}>
+      <Async.Pending>
+        <WaitingPage>
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </WaitingPage>
+      </Async.Pending>
+      <Async.Rejected>{error =>
+        <WaitingPage>
+          <h1>Error Contacting Server: <span className="text-danger">{error.message}</span></h1>
+        </WaitingPage>
+      }</Async.Rejected>
+      <Async.Fulfilled<string>>{data =>
+        <NotAuthenticatedHandler setApiKey={props.setApiKey} authServerUrl={data} />
+      }</Async.Fulfilled>
+    </Async>
   }
 }
 
