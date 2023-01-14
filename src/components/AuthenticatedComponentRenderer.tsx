1e229ca87b1aebb2701513f5c43437663bdc934c
gpi <gpimpale29@gmail.com>
2023-01-13 16:19:24 -0800

    works now

--- src/components/AuthenticatedComponentRenderer.tsx
+++ src/components/AuthenticatedComponentRenderer.tsx
@@ -3,6 +3,7 @@ import { Card, Button } from "react-bootstrap";
 import { ApiKey, Email } from "@innexgo/frontend-auth-api";
 import AuthenticatedComponentProps from '../components/AuthenticatedComponentProps';
 import { Branding } from '@innexgo/common-react-components';
+import { useSearchParams } from "react-router-dom";
 
 export interface AuthenticatedComponentRendererProps {
   branding: Branding,
@@ -17,27 +18,30 @@ function AuthenticatedComponentRenderer({ branding, component: AuthenticatedComp
     apiKey.creationTime + apiKey.duration > Date.now() &&
     apiKey.apiKeyKind === "VALID";
 
+  React.useEffect(() => {
+    if (!isAuthenticated) {
+      const searchParams = new URLSearchParams(window.location.search);
+      // if not authenticated then we need to check if we have a query string containing the api key.
+      const apiKeyJson = searchParams.get('apiKey');
+
+      if (apiKeyJson === null) {
+        // if null we need to redirect to the auth site to login
+        let url = new URL('/login', authServerUrl);
+        url.searchParams.append('src', window.location.href);
+        window.location.replace(url);
+      } else {
+        // if not null then we set api key and restore parameters
+        setApiKey(JSON.parse(apiKeyJson));
+        window.location.replace(searchParams.get("src")!);
+      }
+    }
+  }, [isAuthenticated]);
+
   if (isAuthenticated) {
     return <AuthenticatedComponent apiKey={apiKey!} setApiKey={setApiKey} authServerUrl={authServerUrl} branding={branding} />
-  }
-
-  // get params (if this is a return, then we can just parse)
-  const params = new URLSearchParams(window.location.search);
-
-  // if not authenticated then we need to check if we have a query string containing the api key.
-  const apiKeyJson = params.get('apiKey');
-
-  if (apiKeyJson === null) {
-    // if null we need to redirect to the auth site to login
-    let url = new URL('/login', authServerUrl);
-    url.searchParams.append('src', window.location.href);
-    window.location.replace(url);
   } else {
-    // if not null then we set api key and restore parameters
-    setApiKey(JSON.parse(apiKeyJson));
-    window.history.replaceState(null, '', params.get("src")!);
+    return null;
   }
-  return null;
 }
 
 export default AuthenticatedComponentRenderer;
